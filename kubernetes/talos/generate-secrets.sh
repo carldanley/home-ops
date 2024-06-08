04ccb8a264b2411b6d9a5f34905af5b37b15284b
Carl Danley <carldanley@gmail.com>
2024-06-08 05:01:38 -0400

    fix: massive cleanup on talos configs

--- kubernetes/talos/generate-secrets.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Generate new secrets for this cluster
-talosctl gen secrets -o talsecret.sops.yaml --force
--- kubernetes/talos/reset.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-talosctl reset --reboot=true -n 192.168.10.41
-talosctl reset --reboot=true -n 192.168.10.42
-talosctl reset --reboot=true -n 192.168.10.43
-talosctl reset --reboot=true -n 192.168.10.44
-talosctl reset --reboot=true -n 192.168.10.45
